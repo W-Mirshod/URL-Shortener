74a72fef940af134607bcb1d2cc992ddb4d1d1e3
W Mirshod <python3526@gmail.com>
2025-11-04 03:46:53 +0500

    Update docker-compose.yml and database.py to adjust volume paths and database URL for improved data management

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./url_shortener.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:////app/data/url_shortener.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
